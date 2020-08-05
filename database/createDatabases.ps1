da01d659aaa02a3307a87efcd22396654426661c
Mohit Seth <mohit.seth@uipath.com>
2020-08-05 17:19:06 +0530

    Release v20.7.2

---
+++ database/createDatabases.ps1
@@ -0,0 +1,173 @@
+﻿#Requires -RunAsAdministrator
+
+<#   
+.SYNOPSIS   
+   Create Databases Required for AIFabric
+.DESCRIPTION 
+   Create Databases required by AIFabric i.e ai_helper, ai_pkgmanager, ai_deployer & ai_trainer & the script also creates an user which has db_owner privileges on all these 4 databases 
+   and the username and password that are generated are both logged to console as well as stored as an file in the current directory from where the script execution is triggered.
+.NOTES   
+    Name: ./createDatabases.ps1
+    Author: AIFabric Team
+    Pre-Requisites: script has to be executed throuh powershell in Administrator mode & before running script set execution policty to RemoteSigned by running "Set-ExecutionPolicy RemoteSigned"
+.EXAMPLE 
+    If SQL Server can be accessed through Windows Authentication then:
+    ./createDatabases.ps1 -sqlinstance "DESKTOP-LOUPTI1\SQLEXPRESS" -windowsAuthentication "Y" 
+
+    If SQL Server has to be accessed through SQL Server Authentication:
+    ./createDatabases.ps1 -sqlinstance "DESKTOP-LOUPTI1\SQLEXPRESS" -windowsAuthentication "N" 
+
+    If you wish to change the dbUser Name generated:
+    ./createDatabases.ps1 -sqlinstance "DESKTOP-LOUPTI1\SQLEXPRESS" -windowsAuthentication "N" -dbuser "aifadmin"
+
+    If you wish to generate DB Names with suffixes, this is just for testing purpose:
+    ./createDatabases.ps1 -sqlinstance "DESKTOP-LOUPTI1\SQLEXPRESS" -windowsAuthentication "N" -dbuser "aifadmin" -suffix "_tbd"
+#>
+
+Param (
+   [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName)]
+   [string] $sqlinstance,
+   [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName)]
+   [string] $windowsAuthentication,
+   [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName)]
+   [string] $dbuser,
+   [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName)]
+   [string] $suffix
+)
+
+
+if($windowsAuthentication.Length -eq 0){
+    write-host "since windowsAuthentication flag is not set, defaulting to SQL server authentication"
+    $windowsAuthentication = "N"
+}
+
+if($windowsAuthentication -eq "Y"){
+  echo "Authenticating via Windows Authentication(i.e Using Integrated Auth)"    
+} elseif($windowsAuthentication -eq "N"){
+    $sqlCredentials = Get-Credential
+    #Password Validation
+    $username = $sqlCredentials.username
+    $password = $sqlCredentials.GetNetworkCredential().password
+
+    if(($username.Length -eq 0) -or ($password.Length -eq 0)){
+    write-host "SQL Server Auth Enabled and credentials are not supplied so exiting the program" -ForegroundColor Red
+    exit 1
+    }
+} else{
+    echo "Invalid Input, Exiting the program !, windowsAuthentication should be set to either Y or N"
+    exit
+}
+
+#Validating Input
+if($suffix.Length -gt 0){
+    $suffix = $suffix.ToString()
+}
+
+if($dbuser.Length -gt 0){
+    $dbuser = $dbuser.ToString()
+} else {
+    $dbuser = "aifadmin"
+}
+
+#Generate Random Password
+function generatePassword(){
+    $MinimumPasswordLength = 8
+    $MaximumPasswordLength = 12
+    $PasswordLength = Get-Random -InputObject ($MinimumPasswordLength..$MaximumPasswordLength)
+    $AllowedPasswordCharacters = [char[]]'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?@#$%^&*'
+    $Regex = "(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)"
+
+    do {
+            $Password = ([string]($AllowedPasswordCharacters |
+            Get-Random -Count $PasswordLength) -replace ' ')
+       }    until ($Password -cmatch $Regex)
+
+    $Password
+}
+
+#Execute query on target database
+function executeQuery($database, $query){
+   try{
+       if ($windowsAuthentication -eq "Y"){
+        Invoke-Sqlcmd -ServerInstance $sqlinstance -Database $database -Query $query -ErrorAction Stop -Verbose
+        write-host $query "Succeded" -ForegroundColor Yellow
+        } else{
+        Invoke-Sqlcmd -ServerInstance $sqlinstance -Database $database -Credential $sqlCredentials -Query $query -ErrorAction Stop -Verbose
+        write-host $query "Succeded" -ForegroundColor Yellow
+        }
+     } catch {
+        write-host "Exception type:  $_.Exception.GetType().FullName" -ForegroundColor Red
+        return "Failed"
+   }
+}
+
+#Install modules required
+$installed = Test-Path -Path 'C:\Program Files\WindowsPowerShell\Modules\SqlServer'
+if(!$installed){
+    echo "Installing required module..."
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+	Install-Module -Name SqlServer -AllowClobber -Confirm:$False -Force
+}
+
+Set-ItemProperty -Path 'HKLM:\\SOFTWARE\\Wow6432Node\\Microsoft\\.NetFramework\\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord -Force
+Set-ItemProperty -Path 'HKLM:\\SOFTWARE\\Microsoft\\.NetFramework\\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord -Force
+
+$sqlcommand = "
+GO
+CREATE LOGIN $dbuser WITH PASSWORD=N'{{pwd}}'
+GO
+"
+
+$addUserToMasterQuery = "
+GO
+CREATE USER $dbuser FOR LOGIN $dbuser
+GO
+"
+
+$createDatabaseQuery = "CREATE DATABASE {{DB}}"
+
+
+$grantcommand = "
+GO
+CREATE USER $dbuser FOR LOGIN $dbuser
+GO
+ALTER ROLE [db_owner] ADD MEMBER $dbuser
+GO
+"
+
+$unsecurepassword = generatePassword
+$sqlcommand = $sqlcommand.Replace("{{pwd}}",$unsecurepassword)
+
+#Create Login
+$exitStatus = executeQuery master $sqlcommand
+echo $exitStatus
+
+if ( $exitStatus -eq "Failed" ) {
+    $msg = $Error[0].Exception.Message
+    write-host "Encountered error while creating login. Error Message is $msg." -ForegroundColor Red
+    write-host "If the login already exists and if the login is generated via this script the same can be found under the file name $dbuser located in the current directory" -ForegroundColor Red
+    write-host "If you are using WindowsAuthentication mode, make sure that you are using ServerName instead of IP" -ForegroundColor Red
+} else {
+    echo "the PASSWORD for $dbuser user is : $unsecurepassword "
+    New-Item -ItemType File -Force -Path ".\$dbuser" -Value $unsecurepassword
+}
+
+#Add login to Master
+$exitStatus = executeQuery master $addUserToMasterQuery
+
+if ( $exitStatus -eq "Failed" ) {
+    write-host "The target database does not support adding user to master anyhow this wont affect user from logging into DB with the generated credentials" -ForegroundColor Yellow
+}
+
+#Create DB's
+executeQuery master $createDatabaseQuery.Replace("{{DB}}", "ai_helper$suffix")
+executeQuery master $createDatabaseQuery.Replace("{{DB}}", "ai_deployer$suffix")
+executeQuery master $createDatabaseQuery.Replace("{{DB}}", "ai_pkgmanager$suffix")
+executeQuery master $createDatabaseQuery.Replace("{{DB}}", "ai_trainer$suffix")
+
+
+#Execute Grants
+executeQuery "ai_helper$suffix" $grantcommand
+executeQuery "ai_deployer$suffix" $grantcommand
+executeQuery "ai_pkgmanager$suffix" $grantcommand
+executeQuery "ai_trainer$suffix" $grantcommand
---
+++ orchestrator/orchestratorAutomation.ps1
@@ -0,0 +1,239 @@
+﻿#Requires -RunAsAdministrator
+
+<#   
+
+.SYNOPSIS   
+   Makes aifabric related changes to orchestrator web.config to enable aifabric installation and access.
+.DESCRIPTION 
+   Add entries in orchestrator web.config(if not exists) for orchestrator internal IDP and aifabric access from robot and orchestrator. 
+   Removes cache to allow access to new controllers and resets iis to load new values.
+.NOTES   
+    Name: ./orchestratorAutomation.ps1
+    Author: AIFabric Team
+    Pre-Requisites: script has to be executed throuh powershell in Administrator mode & before running script set execution policy to RemoteSigned by running "Set-ExecutionPolicy RemoteSigned"
+.EXAMPLE 
+    If orchestrator is hosted at orchestrator.uipath.com and aifabric is available at ww.xx.yy.zz, command to run would be 
+    ./orchestratorAutomation.ps1 -aifip ww.xx.yy.zz -orcname orchestrator.uipath.com
+
+#>
+
+Param (
+   [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName)]
+   [string] $orcname,
+   [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName)]
+   [string] $config,
+   [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName)]
+   [string] $aifip,
+   [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName)]
+   [string] $aifport
+)
+
+Import-Module 'WebAdministration'
+    if(!$config)
+    {   
+        $config = "C:\Program Files (x86)\UiPath\Orchestrator\web.config"
+    }
+    if(!$aifport)
+    {   
+        $aifport = "31390"
+    }
+
+echo "Path to web.config: "$config
+
+Copy-Item $config -Destination ("$config.original."+(Get-Date -Format "MMddyyyy.HH.mm.ss"))
+
+#AiFabric Settings template
+$STATIC_NODES_STRING='
+<Collection>
+    <add key="CertificatesStoreLocation" value="LocalMachine" />
+    <add key="AiFabric.Licensing" value="true" />
+    <add key="AiFabric.MLSkillsCreate" value="false" />
+    <add key="AiFabric.MLSkillsCreateOOB" value="false" />
+    <add key="AiFabric.PackagesCreate" value="false" />
+    <add key="AiFabric.Packages" value="false" />
+    <add key="AiFabric.Logs" value="false" />
+    <add key="AiFabric.ModuleEnabled" value="true" />
+    <add key="AiFabric.FeatureEnabledByDefault" value="true" />
+    <add key="AiFabric.ModelStorageUrl" value="https://{{aifip}}:31443" />
+    <add key="AiFabric.MLPackagingInstructionsUrl" value="something" />
+    <add key="AiFabric.MLServiceUrl" value="https://{{aifip}}:{{aifport}}" />
+    <add key="AiFabric.MLSkillUrl" value="https://{{aifip}}:{{aifport}}/ai-deployer" />
+    <add key="AiFabric.MLPackageUrl" value="https://{{aifip}}:{{aifport}}/ai-pkgmanager" />
+    <add key="AiFabric.MLLogUrl" value="https://{{aifip}}:{{aifport}}/ai-helper" />
+    <add key="AiFabric.MLTrainUrl" value="https://{{aifip}}:{{aifport}}/ai-trainer" />
+    <add key="AiFabric.AccountId" value="host" />
+    <add key="IDP.Scope" value="[&quot;AIFabric&quot;,&quot;Orchestrator&quot;]" />
+    <add key="IDP.CurrentTokenThumbprint" value="{{thumbprint}}" />
+    <add key="IDP.PreviousTokenThumbprint" value="{{thumbprint}}" />
+    <add key="IDP.SigningCertificate" value="{{cert}}" />
+    <add key="IDP.Authority" value="https://{{orchestratorhostname}}/api/auth/" />
+    <add key="IdentityProviderFeature.Enabled" value="true" />
+    <add key="Auth.OAuth.SharedRobotOAuthClientId" value="03FFA863-3C0C-4EEC-BBE5-094D4FCF4F22" />
+    <add key="Auth.OAuth.SharedRobotOAuthAuthority" value="https://{{orchestratorhostname}}/api/auth/" />
+    <add key="Auth.OAuth.SharedOrchestratorOAuthClientId" value="a42436d5-4cd6-4d6a-9311-51271d9fc217" />
+    <add key="Auth.OAuth.SharedOrchestratorOAuthAuthority" value="https://{{orchestratorhostname}}/api/auth/" />
+    <add key="Auth.OAuth.OrchestratorOAuthAudience" value="Orchestrator" />
+    <add key="Auth.OAuth.RobotAuthenticationEnabled" value="true" />
+    <section name="ClientApplications" type="UiPath.Orchestrator.Security.IdentityProvider.Model.ClientApplications, UiPath.Orchestrator.Security.IdentityProvider" />
+</Collection>'
+
+#Client App Settings
+$CLIENT_APP_NODES_STRING='
+<Collection>
+    <ClientApplications>
+        <add displayName="Robot" clientId="03ffa863-3c0c-4eec-bbe5-094d4fcf4f22" jwtExpirationInSeconds="86400"/>
+        <add displayName="Orchestrator" clientId="a42436d5-4cd6-4d6a-9311-51271d9fc217" jwtExpirationInSeconds="86400"/>
+    </ClientApplications>
+</Collection>'
+
+# get orchestrator cert values 
+$thumbrint = $null
+$RawBase64 = $null
+$orchestratorhostname = $null
+Get-ChildItem -Path IIS:SSLBindings | ForEach-Object -Process `
+{
+    if ($_.Sites)
+    {
+        $_.Sites
+        $sname = $_.Sites.Value
+        if($config.Trim().ToLower().Equals((Get-WebFilePath "IIS:\Sites\$sname\web.config").ToString().Trim().ToLower()))
+        {
+            $certificate = Get-ChildItem -Path CERT:LocalMachine/My |
+                Where-Object -Property Thumbprint -EQ -Value $_.Thumbprint
+            $thumbprint = $certificate.Thumbprint.ToLower()
+            $RawBase64 = [System.Convert]::ToBase64String($certificate.GetRawCertData())
+            $orchestratorhostname = ((Get-WebBinding -Name $sname) | where{$_.protocol -eq "https"}).bindingInformation.Split(":")[-1]
+        }
+    }
+}
+
+# validations of input 
+if($orcname)
+{
+    if ($orcname.StartsWith("http://") -or $orcname.StartsWith("https://"))
+    {
+        echo "orcname should not start with http or https"
+        throw "Invalid orcname input provided: $orcname"
+    }
+    $orchestratorhostname = $orcname
+}
+if($aifip.StartsWith("http://") -or $aifip.StartsWith("https://"))
+{
+    echo "aifip should not start with http or https"
+    throw "Invalid aifip input provided: $aifip"   
+}
+
+# set nodes value
+$STATIC_NODES_STRING = $STATIC_NODES_STRING.Replace("{{aifip}}",$aifip);
+$STATIC_NODES_STRING = $STATIC_NODES_STRING.Replace("{{thumbprint}}",$thumbprint);
+$STATIC_NODES_STRING = $STATIC_NODES_STRING.Replace("{{cert}}",$RawBase64);
+$STATIC_NODES_STRING = $STATIC_NODES_STRING.Replace("{{orchestratorhostname}}",$orchestratorhostname);
+$STATIC_NODES_STRING = $STATIC_NODES_STRING.Replace("{{aifport}}",$aifport);
+$STATIC_NODES = [xml]$STATIC_NODES_STRING
+$CLIENT_APP_NODES = [xml]$CLIENT_APP_NODES_STRING
+
+# edit web config
+function AifabricFixedConfig
+{
+    $nodes = Select-Xml -XPath '//add' -Xml $STATIC_NODES
+    $additionalSection = Select-Xml -XPath '//section' -Xml $STATIC_NODES
+    $ClientApplicationsSection = Select-Xml -XPath '//ClientApplications' -Xml $CLIENT_APP_NODES
+    
+    $file = gi $config
+    $xml = [xml](gc $file)
+    foreach($node in $nodes)
+    {
+        #remove existing nodes if they exist. They should not. 
+        $key = $node.Node.key
+        $xml.SelectNodes("configuration/appSettings/add[@key='$key']") | %{$xml.configuration.appSettings.RemoveChild($_)}
+        $xml.configuration.appSettings.AppendChild($xml.ImportNode($node.Node,1))
+    }
+    if($xml.SelectNodes("configuration/configSections/section[@name='ClientApplications']").Count -eq 0)# | %{$xml.configuration.configSections.RemoveChild($xml.ImportNode($additionalSection.Node,1))}
+    {
+        $xml.configuration.configSections.AppendChild($xml.ImportNode($additionalSection.Node,1))
+    }
+    if($xml.SelectNodes("configuration/ClientApplications").Count -eq 0)
+    {
+        $xml.configuration.AppendChild($xml.ImportNode($ClientApplicationsSection.Node,1))
+    }
+    $xml.Save($file.FullName)
+}
+
+# iisreset
+function Retry-IISRESET
+{
+    param (
+    [Parameter(Mandatory=$false)][int]$retries = 5, 
+    [Parameter(Mandatory=$false)][int]$secondsDelay = 2
+    )
+        
+    $retrycount = 0
+    $completed = $false
+
+    while (-not $completed) {
+        try {
+            iisreset | Tee-Object -Variable statusiisreset
+			if("$statusiisreset".Contains("failed"))
+			{
+				throw
+			}
+            Write-Verbose ("reset succeded")
+            $completed = $true
+        } catch {
+            if ($retrycount -ge $retries) {
+                throw
+            } else {
+                Start-Sleep $secondsDelay
+                $retrycount++
+            }
+        }
+    }
+}
+
+# remove cache
+function EmptyAspNetCache
+{
+    $Framework32bitFolder = "\Framework\"
+    $Framework64bitFolder = "\Framework64\"
+    $temporaryAspNetFolder = "Temporary ASP.NET Files\root"
+    $ControllerCacheFileName = "MS-ApiControllerTypeCache.xml"
+    $aspNetCacheFolder = [System.Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()+$temporaryAspNetFolder
+    if ([Environment]::Is64BitOperatingSystem)
+    {
+        $aspNetCacheFolder = $aspNetCacheFolder.Replace($Framework32bitFolder, $Framework64bitFolder);
+    }
+    if (!(Test-Path $aspNetCacheFolder))
+    {
+        echo $"Folder $aspNetCacheFolder not found for removing $ControllerCacheFileName"
+        return
+    }
+    echo $"Removing $ControllerCacheFileName files from ASP.NET cache folder $aspNetCacheFolder"
+    Get-Childitem –Path $aspNetCacheFolder -Include $ControllerCacheFileName -Recurse | ForEach {
+        $retrycount = 0
+        $retries = 3
+        $completed = $false
+
+        while (-not $completed) {
+            try {
+                Remove-Item $_.FullName
+                $completed = $true
+            } catch {
+                if ($retrycount -ge $retries) {
+                    throw
+                } else {
+                    Start-Sleep 2
+                    $retrycount++
+                }
+            }
+        }
+       
+        echo $"Removed $ControllerCacheFileName"
+    }
+}
+
+#create the proper web.config with configuration
+AifabricFixedConfig
+EmptyAspNetCache
+Retry-IISRESET 3 2
+Sleep 2
+echo "Orchestrator configured successfully"
