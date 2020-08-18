48a1259ff06fd555c1e87c23601e59c82fc1a4af
Mohammed Irfan Ullah <mohammed.irfan@uipath.com>
2020-08-18 19:30:38 +0530

    updating http to https

--- gpu/install_gpu_drivers.sh
+++ gpu/install_gpu_drivers.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 function install_gpu() {
-    curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+    curl -O https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
     sudo dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
-    sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+    sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 
         sudo apt update
         sudo apt-get -y install cuda
