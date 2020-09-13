bf5f87d93484867544c4f210cee97fe7c9b0cef5
rajivml <46923567+rajivml@users.noreply.github.com>
2020-09-13 17:23:37 +0530

    change the way kubeconfig is loaded

--- platform/scripts/create-serviceaccount.sh
+++ platform/scripts/create-serviceaccount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Creating provision service account"
-bash -l
 chmod +r /etc/kubernetes/admin.conf
-kubectl create -n default serviceaccount provision 
+export KUBECONFIG=/etc/kubernetes/admin.conf
+kubectl create -n default serviceaccount provision
 kubectl create -n default clusterrolebinding provision-admin --clusterrole=cluster-admin --serviceaccount=default:provision
