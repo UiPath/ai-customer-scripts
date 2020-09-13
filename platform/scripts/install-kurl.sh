5baaf045a1154a8bb4077fe9ed95fceca66eb1ca
rajivml <46923567+rajivml@users.noreply.github.com>
2020-09-13 16:31:12 +0530

    change permissions

--- platform/scripts/install-kurl.sh
+++ platform/scripts/install-kurl.sh
@@ -2,6 +2,6 @@
 
 curl -sSL https://k8s.kurl.sh/aif-core-oneboxinstaller | sudo bash
 bash -l
-sudo chmod +r /etc/kubernetes/admin.conf
+chmod +r /etc/kubernetes/admin.conf
 kubectl create -n default serviceaccount provision 
 kubectl create -n default clusterrolebinding provision-admin --clusterrole=cluster-admin --serviceaccount=default:provision
