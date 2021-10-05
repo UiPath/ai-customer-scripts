c95d3257ea0438b911f73c5094b7581d757e6dc4
addyGarg <76091894+addyGarg@users.noreply.github.com>
2021-10-05 12:43:28 +0530

    Added dataset and mlpacakges migration script to master script

--- platform/sfmigration/mastermigrationscript.bash
+++ platform/sfmigration/mastermigrationscript.bash
@@ -11,11 +11,15 @@ green=$(tput setaf 2)
 yellow=$(tput setaf 3)
 default=$(tput sgr0)
 
+## TODO: should we use absolute path at every location.
 readonly CREDENTIALS_FILE=$1
 export BASE_PATH=$2
+readonly ABSOLUTE_BASE_PATH=$(pwd)
 
 # Validate file provided by user exists or not, It may be relative path or absolute path
 # $1 - File path
+
+# TODO: Add more file paths to be validated.
 function validate_file_path() {
   if [ ! -f "$1" ]; then
     echo "$red $(date) $1 file does not exist, Please check ... Exiting $default"
@@ -53,17 +57,33 @@ function db_migration(){
  sh ./databasemigration/dbmigration.bash $1 $2 $3 $4
 }
 
+function dataset_and_mlpackages_migration(){
+  # TODO: Do we need to change the extension from sh to bash?
+  ## run export script
+  sh ./storagemigration/export.sh ./storagemigration/SOURCE_CREDENTIAL_FILE $ABSOLUTE_BASE_PATH/storagemigration/storageData
+
+  ## Run sanitize script.
+  sh ./storagemigration/sanitize.sh $ABSOLUTE_BASE_PATH/storagemigration/storageData/ceph/ml-model-files
+
+  ## Run import script.
+  sh ./storagemigration/import.sh ./storagemigration/TARGET_CREDENTIALS_FILE $ABSOLUTE_BASE_PATH/storagemigration/storageData $1 $2 $3
+}
+
 # Function to validate credential file and perform database migration for each tenant
 # $1 - Credentials file
-function parse_input_and_migrate_db() {
+function parse_input_and_migrate() {
 
 jq -c '.TENANT_MAP[]' $CREDENTIALS_FILE | while read tenantEntry; do
     # Loop through each element of array
+  # TODO: why we are exporting the variables when we are passing them as an argument.
 	export SRC_TENANT_ID=$(echo $tenantEntry | jq -r '.SRC_TENANT_ID')
 	export DESTINATION_TENANT_ID=$(echo $tenantEntry | jq -r '.DESTINATION_TENANT_ID')
 	export DESTINATION_ACCOUNT_ID=$(echo $tenantEntry | jq -r '.DESTINATION_ACCOUNT_ID')
+	BUCKET_NAME="train-data"
 	db_migration $CREDENTIALS_FILE $SRC_TENANT_ID $DESTINATION_TENANT_ID $DESTINATION_ACCOUNT_ID
-	echo "$(date) Successfully parsed input file and db migration completed"
+	echo "$(date) Successfully parsed input file and db migration completed."
+	dataset_and_mlpackages_migration $SRC_TENANT_ID $DESTINATION_TENANT_ID $BUCKET_NAME
+	echo "$(date) Successfully migrated datasets and mlpackages."
 done
 }
 
@@ -77,13 +97,4 @@ validate_file_path $CREDENTIALS_FILE
 validate_setup
 
 # Trigger DB migration script
-parse_input_and_migrate_db
-
-
-# get credentials --> SF , replicated --> get credentil 10.19.
-
-# export SOURCE_CREDENTIAL_FILE storageData
-
-# exportMLPackage storageData/ceph/ml-model-files
-
- # import DEST_CREDENTIAL_FILE storage
+parse_input_and_migrate
