93c8a59290fbad34e4ea245618126c77c32dbadb
AdiNicuPopa <45226692+AdiNicuPopa@users.noreply.github.com>
2025-03-24 16:07:37 +0200

    Adding 23.4.12 metadatas (#371)

--- metadata_generate_new_du_versions.ps1
+++ metadata_generate_new_du_versions.ps1
@@ -9,13 +9,13 @@ $folderPath = $PSScriptRoot + "/metadata"
 
 echo $folderPath
 
-$previousCustomVersion = '23.10.8'
+$previousCustomVersion = '23.4.11'
 # Set the new custom version number
-$newCustomVersion = '23.10.9'
+$newCustomVersion = '23.4.12'
 
 # Set the text to be replaced and the replacement text
 $targetImage = '' # leave empty to generate metadata for all the models. Possible non empty values: du-semistructured, du-doc-ocr, du-doc-ocr-cpu, du-ml-document-type-text-classifier
-$newTag = 'v23.10-03.14-rc02'
+$newTag = 'v23.4-03.12-rc02'
 
 # Get a list of all files in the folder that match the specified format
 $fileList = Get-ChildItem $folderPath | Where-Object { $_.Name -match "^([a-zA-Z0-9_]+)__([0-9]+)__metadata\.json$" }
